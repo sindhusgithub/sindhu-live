f2d7704692f01c6e5465fbd332506b694da99611
sindhu <work@sindhushivaprasad.com>
2026-07-14 18:37:52 +0530

    Quartz sync: Jul 14, 2026, 6:37 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Source Serif 4",
-        body: "Source Serif 4",
+        header: "Alegreya Sans",
+        body: "Alegreya",
         code: "IBM Plex Mono",
       },
       colors: {
