8b3a51bf0489c21823bf1c9764d3b7b60aae9455
Sindhu Shivaprasad <102961770+sindhusgithub@users.noreply.github.com>
2026-09-11 15:53:06 +0530

    Use explicit local font loading

--- quartz.config.ts
+++ quartz.config.ts
@@ -21,8 +21,8 @@ const config: QuartzConfig = {
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
-      fontOrigin: "googleFonts",
-      cdnCaching: true,
+      fontOrigin: "local",
+      cdnCaching: false,
     typography: {
         header: "Newsreader",
         body: "Newsreader",
